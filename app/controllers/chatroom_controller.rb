2a94bda80307c978f870b04f85fe13dff8bb0352
adamschueller <aschueller@zoominternet.net>
2019-01-03 15:02:42 -0500

    completed project

--- app/controllers/chatroom_controller.rb
+++ app/controllers/chatroom_controller.rb
@@ -1,6 +1,9 @@
 class ChatroomController < ApplicationController
+  before_action :require_user
+
   def index
     @messages = Message.all
     @users = User.all
+    @message = Message.new
   end
 end
---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,19 @@
+class MessagesController < ApplicationController
+  before_action :require_user
+
+  def create
+    message = current_user.messages.build(message_params)
+    if message.save
+      ActionCable.server.broadcast "chatroom_channel", message: render_message(message), chef: message.chef.chefname
+    else
+      redirect_to root_path
+    end
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:body)
+  end
+
+end
